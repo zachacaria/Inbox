60291502433bc4afb0dbfcf39dad5cf42f145ff4
zachacaria <zach.e.garcia@gmail.com>
2018-01-19 19:33:27 -0600

    Update

--- Test/Inbox.test.js
+++ Test/Inbox.test.js
@@ -4,31 +4,24 @@ const Web3 = require('web3');
 //^^constructor
 const web3 = new Web3(ganache.provider());
                       //will change based on what newtwork is being used
+const { interface, bytecode } = require('../compile');
 
-/*class Car {
-  park(){
-    return 'stopped';
-  }
+let accounts;
+let inbox;
 
-  drive(){
-    return 'vroom';
-  }
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
 
-}
-
-let car;
-
-beforeEach(() => {
-  car = new Car();
+  // Use one of those accounts to deploy
+  // the coontract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' })
 });
 
-describe('Car', () => {
-  it('can park', () => {
-    assert.equal(car.park(), 'stopped');
-  });
-
-  it('can drive', () => {
-    assert.equal(car.drive(), 'vroom');
-  });
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(inbox);
+  });;
 });
-*/
