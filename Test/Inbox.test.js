c1672a6e056feae4ec373533840c6b1251e83a59
zachacaria <zach.e.garcia@gmail.com>
2018-01-19 22:19:22 -0600

    updated

--- Test/Inbox.test.js
+++ Test/Inbox.test.js
@@ -2,7 +2,9 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 //^^constructor
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
                       //will change based on what newtwork is being used
 const { interface, bytecode } = require('../compile');
 
@@ -18,10 +20,23 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' })
+
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
-  });;
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  });
 });
